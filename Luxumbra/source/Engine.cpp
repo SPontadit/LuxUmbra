cf4ed49f828dde580a0526d4920c5671af0c5320
M_deLaharpe <matthieu.delaharpe@gmail.com>
2019-10-09 20:27:35 +0200

    readded files for rename

---
+++ Luxumbra/source/Engine.cpp
@@ -0,0 +1,28 @@
+#include "Engine.h"
+
+namespace lux
+{
+	Engine::Engine()
+		: isInitialized(false), window(), rhi()
+	{
+
+	}
+
+	Engine::~Engine()
+	{
+
+	}
+
+	bool Engine::Initialize(uint32_t windowWidth, uint32_t windowHeight)
+	{
+		if (!window.Initialize(windowWidth, windowHeight))
+			return false;
+
+		if (!rhi.Initialize(window))
+			return false;
+
+		isInitialized = true;
+
+		return true;
+	}
+}
