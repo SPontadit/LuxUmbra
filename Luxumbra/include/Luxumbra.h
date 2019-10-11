619e1b260f3c70ea63dd41e49f9d8ca874657f27
Stephane Pontadit <stephane.pontadit@gmail.com>
2019-10-11 11:58:49 +0200

    Added a macro ASSERT in luxumbra.h

--- Luxumbra/include/Luxumbra.h
+++ Luxumbra/include/Luxumbra.h
@@ -8,4 +8,11 @@
 #define TO_SIZE_T(x) static_cast<size_t>(x)
 #define TO_UINT32_T(x) static_cast<uint32_t>(x)
 
+#ifdef _DEBUG
+#include <cassert>
+#define ASSERT(x) if(x == false) { __debugbreak(); assert(false); }
+#else // !_DEBUG
+#define ASSERT(x)
+#endif // _DEBUG
+
 #endif // LUXUMBRA_H_INCLUDED
